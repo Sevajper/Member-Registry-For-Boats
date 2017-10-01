c81607b44357230dc3c664105f4e2b0e0133a7e4
Pranav <pranav.dilip.patel@gmail.com>
2017-10-01 13:06:32 +0200

    more update to registrycontroller

--- src/controller/RegistryController.java
+++ src/controller/RegistryController.java
@@ -17,17 +17,18 @@ public class RegistryController {
 	public ArrayList<Member> memberList = new ArrayList<Member>();
 	String desktop = System.getProperty("user.home");
 	File file = new File(desktop);
-	
 
+	// Take all the members in the memberList ArrayList and put them into a file on
+	// the desktop
 	public void saveToRegistry() throws IOException {
 		BufferedWriter out = new BufferedWriter(new FileWriter(file));
-		
+
 		try {
-		JAXBContext context;
-		context = JAXBContext.newInstance(Member.class);
-		Marshaller marshaller = context.createMarshaller();
-		marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-		
+			JAXBContext context;
+			context = JAXBContext.newInstance(Member.class);
+			Marshaller marshaller = context.createMarshaller();
+			marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+
 			marshaller.marshal(memberList, out);
 		} catch (JAXBException e) {
 			e.printStackTrace();
@@ -35,18 +36,20 @@ public class RegistryController {
 
 	}
 
-	public ArrayList loadFromRegistry() {
+	// Take the members from the file and load them into the ArrayList registry
+	public ArrayList<Member> loadFromRegistry() {
 		try {
-		JAXBContext context = JAXBContext.newInstance(Member.class);
-		Unmarshaller unMarshaller = context.createUnmarshaller();
+			JAXBContext context = JAXBContext.newInstance(Member.class);
+			Unmarshaller unMarshaller = context.createUnmarshaller();
 
-		return (ArrayList) unMarshaller.unmarshal(file);
+			return (ArrayList<Member>) unMarshaller.unmarshal(file);
 		} catch (JAXBException e) {
 			e.printStackTrace();
 		}
 		return null;
 	}
 
+	// IDK what this method does yet
 	public void getMembers() {
 
 	}
